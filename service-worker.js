f7e8dcb7251f98a24b6859ebb1b1f9cf335c40b9
bonford <mark.mcbrearty@gmail.com>
2025-07-04 11:23:09 -0400

    First Commit

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const cacheName = 'random-touch-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
